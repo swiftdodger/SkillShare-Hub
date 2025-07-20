d0a0017dd043e189da63a3bdb25e9d8d016e3613
swiftdodger <166481471+swiftdodger@users.noreply.github.com>
2025-07-20 11:49:15 +0300

    fixing redirecting after registration

--- users/views.py
+++ users/views.py
@@ -9,7 +9,7 @@ def register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('dashboard')
+            return redirect('role_redirect')
     else:
         form = RegisterForm()
     return render(request, 'register.html', {'form': form})
