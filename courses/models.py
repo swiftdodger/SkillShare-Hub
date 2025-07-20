82f94a33314944467b5fa7644dea0a7497cb90da
swiftdodger <166481471+swiftdodger@users.noreply.github.com>
2025-07-20 11:22:46 +0300

    making models inside courses  app

--- courses/models.py
+++ courses/models.py
@@ -17,7 +17,7 @@ class Lesson(models.Model):
     course = models.ForeignKey('Course', on_delete=models.CASCADE, related_name='lessons')
     title = models.CharField(max_length=100)
     content = models.TextField()
-    video = models.fileField(upload_to='course_videos/', blank=True, null=True)
+    video = models.FileField(upload_to='course_videos/', blank=True, null=True)
 
     def __str__(self):
         return f"{self.title} - {self.course.title}"
