519ed559a0ce5de51e0cad2999eaff94b09c7cf5
swiftdodger <166481471+swiftdodger@users.noreply.github.com>
2025-07-20 11:34:03 +0300

    importing stuff into views/courses

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from courses.models import Course, Lesson
+from courses.forms import CourseForm
 # Create your views here.
