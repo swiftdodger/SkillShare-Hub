0de258d278cc8db952e209b227658cf17e3cc9df
swiftdodger <166481471+swiftdodger@users.noreply.github.com>
2025-07-20 11:38:37 +0300

    creating create course/course_list and course_detail urls

---
+++ courses/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path,include
+from courses import views
+
+urlpatterns = [
+    path('', views.course_list, name='course_list'),
+    path('<int:pk>/', views.course_detail, name='course_detail'),
+    path('create/', views.create_course,name='create_course'),
+]
