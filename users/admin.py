2de81f9cc64ee2da15777d8570975b81e5ec9a61
swiftdodger <166481471+swiftdodger@users.noreply.github.com>
2025-07-20 11:05:07 +0300

    admin register inside users app

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'role', 'created_at')
+    list_filter = ('role','created_at')
+    search_fields = ('user__username', 'user__email')
+    ordering = ('-created_at',)
+    list_select_related = ('user',)
