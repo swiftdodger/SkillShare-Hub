3c41a6898e220c6ecefe16977103dfcf5c60c748
swiftdodger <166481471+swiftdodger@users.noreply.github.com>
2025-07-20 11:50:06 +0300

    fixing redirecting after registration

--- users/urls.py
+++ users/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('profile/edit/', views.edit_profile, name='edit_profile'),
     path('redirect-after-login/', views.role_redirect, name='role_redirect'),
     path('dashboard/instructor/', views.instructor_dashboard, name='instructor_dashboard'),
-    path('redirect/', views.role_redirect, name='role_redirect')
+    path('redirect/', views.role_redirect, name='role_redirect'),
 
     path('dashboard/student/', views.student_dashboard, name='student_dashboard'),
 ]
