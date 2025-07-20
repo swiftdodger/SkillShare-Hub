6837a80ab047e031abc4556cf15e2d863ddc9379
swiftdodger <166481471+swiftdodger@users.noreply.github.com>
2025-07-20 11:14:30 +0300

    creating lesson model

--- courses/models.py
+++ courses/models.py
@@ -13,4 +13,11 @@ class Course(models.Model):
     def __str__(self):
         return self.title
 
-    
+class Lesson(models.Model):
+    course = models.ForeignKey('Course', on_delete=models.CASCADE, related_name='lessons')
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    video = models.fileField(upload_to='course_videos/', blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.course.title}"
