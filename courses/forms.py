1e0f0903f25aa1cdaf83fd752d274d4bab63bc29
swiftdodger <166481471+swiftdodger@users.noreply.github.com>
2025-07-20 11:21:25 +0300

    creating course app forms ; Course and lessons

---
+++ courses/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from courses.models import Course,Lesson
+
+class CourseForm(forms.ModelForm):
+    class Meta:
+        model = Course
+        exclude = ('instructor', 'created_at')
+
+class LessonForm(forms.ModelForm):
+    class Meta:
+        model = Lesson
+        fields = '__all__'
