ef8854745ee0a7fd074307d849cfde42d4eaa09c
swiftdodger <166481471+swiftdodger@users.noreply.github.com>
2025-07-20 11:11:20 +0300

    adding course model into models

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    category = models.CharField(max_length=50)
+    instructor = models.ForeignKey('users.UserProfile', on_delete=models.CASCADE, related_name='courses')
+    thumbnail = models.ImageField(upload_to='course_thumbnails/', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+    
