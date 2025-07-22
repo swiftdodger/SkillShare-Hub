317fd215c3a8d7f8eedf70dd03cec72af6cf9b4b
swiftdodger <flaviogabrielbusiness@gmail.com>
2025-07-22 23:07:21 +0300

    adding detail nad list urls in courses/urls

--- courses/urls.py
+++ courses/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.course_list, name='course_list'),
     path('<int:pk>/', views.course_detail, name='course_detail'),
     path('create/', views.create_course,name='create_course'),
+    path('list/', views.course_list, name='course_list'),
+    path('detail/<int:pk>/', views.course_detail, name='course_detail'),
+
 ]
