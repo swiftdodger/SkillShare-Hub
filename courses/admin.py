6952481d094e369eca8785eeba24f7f8d78b0662
swiftdodger <flaviogabrielbusiness@gmail.com>
2025-07-22 14:45:58 +0300

    changing some stuff

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from courses.models import Course, Lesson
 
-# Register your models here.
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'instructor', 'created_at')
+    list_filter = ('category', 'created_at')
+    search_fields = ('title', 'description')
+    ordering = ('-created_at',)
+    autocomplete_fields = ['instructor']
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ('title', 'course')
+    search_fields = ('title', 'course__title')
+    autocomplete_fields = ['course']
